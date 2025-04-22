41cf915e94a8593352d988711598cb56728422ba
Thanaphat Chaiboonrueang <tan.thanaphat4@gmail.com>
2025-04-22 15:29:05 +0700

    Update NewMonoBehaviourScript.cs

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -5,13 +5,13 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print.log("Hello, World!");    
+        Debug.Log("Hello, World!");    
     }
 
     // Update is called once per frame
     void Update()
     {
-        print.log("Hello, World!"); 
+        Debug.Log("Hello, World!"); 
 
     }
 }
