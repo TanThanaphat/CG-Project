c681b24e3e13df8a0a90807c0866d100a8098c5c
Bossomdn <bossnoname2018@gmail.com>
2025-04-27 17:38:47 +0700

    edit car control

--- Assets/Scripts/SteeringIK.cs
+++ Assets/Scripts/SteeringIK.cs
@@ -14,7 +14,7 @@ public class SteeringIK : MonoBehaviour
 
     void OnAnimatorIK(int layerIndex)
     {
-        Debug.Log("IK running!");
+        //Debug.Log("IK running!");
         if (animator)
         {
             // Lean the body forward
